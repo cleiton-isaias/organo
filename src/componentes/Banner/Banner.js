154c2a3db86ebc0ecd82372c800eccb4e2c2433b
cleiton-isaias <cleitonisaias@live.com>
2022-09-02 16:21:06 -0300

    Adicionado link github.

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -4,7 +4,14 @@ export const Banner = () => {
     // JSX
     return (
         <header className="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
-        </header>
+            <>
+                <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+            </>
+            <div className='git'>
+                <a href="https://github.com/cleiton-isaias" target="_blank">
+                    <img src="/imagens/github.png" alt="Github de Cleiton Isaias" />
+                </a>
+            </div>
+        </header >
     )
 }
